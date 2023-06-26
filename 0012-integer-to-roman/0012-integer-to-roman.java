5441d30cbd03709479b9f62c392a43c4bc03aa44
Ashok Kumar Dharanikota <103619560+Ashok-Kumar-dharanikota@users.noreply.github.com>
2023-06-26 17:28:09 +0530

    Time: 7 ms (41.79%), Space: 43 MB (77.56%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String intToRoman(int num) {
+        String Roman = "";
+        Object [][] storeIntRoman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"},                                        {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+        for (int i = 0; i < storeIntRoman.length; i++) {
+            while (num >= (int)storeIntRoman[i][0]) {
+                Roman += (String)storeIntRoman[i][1];
+                num -= (int)storeIntRoman[i][0];
+            }
+        }
+    return Roman;
+    }
+}
